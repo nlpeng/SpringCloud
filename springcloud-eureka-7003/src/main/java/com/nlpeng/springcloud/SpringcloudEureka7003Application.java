300556d6882c13f41f31963e97c9e88743f814ab
nlpeng <nlpeng@outlook.com>
2020-02-04 12:44:21 +0800

    1.2 补全README.mf文件

--- springcloud-eureka-7003/src/main/java/com/nlpeng/springcloud/SpringcloudEureka7003Application.java
+++ springcloud-eureka-7003/src/main/java/com/nlpeng/springcloud/SpringcloudEureka7003Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringcloudEureka7003Application {
+    
 
     public static void main(String[] args) {
         SpringApplication.run(SpringcloudEureka7003Application.class, args);

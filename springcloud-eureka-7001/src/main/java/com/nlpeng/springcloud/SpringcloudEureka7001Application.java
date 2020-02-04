86c4d8c1774b6d962fc56c716a137fd528bda381
nlpeng <nlpeng@outlook.com>
2020-02-04 12:41:37 +0800

    1.2 补全README.mf文件

--- springcloud-eureka-7001/src/main/java/com/nlpeng/springcloud/SpringcloudEureka7001Application.java
+++ springcloud-eureka-7001/src/main/java/com/nlpeng/springcloud/SpringcloudEureka7001Application.java
@@ -4,11 +4,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import java.util.*;
+
 @SpringBootApplication
 @EnableEurekaServer // EurekaServer服务器端启动类,接受其它微服务注册进来
 public class SpringcloudEureka7001Application {
 
     public static void main(String[] args) {
+    
+    
+        List a = new ArrayList();
+        ArrayList b = new ArrayList();
+        a.toArray();
+        b.trimToSize();
+        HashMap hashMap = new HashMap();
+        Set set = hashMap.entrySet();
+        List list = new ArrayList(set);
+        Collections.sort(list, new Comparator<Object>() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                return 0;
+            }
+        });
+    
         SpringApplication.run(SpringcloudEureka7001Application.class, args);
     }
 
